33a25736526a64ff18eae284e7519d8197e90364
Mudura Razvan <razvanmudura25@gmail.com>
2021-12-14 14:18:00 +0200

    small synthax change

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const minutesSpan = document.getElementById("minutes");
 const secondsSpan = document.getElementById("seconds");
 const millisecondsSpan = document.getElementById("milliseconds");
 
-let milliseconds, seconds, minutes, hours;
-let started;
+let milliseconds, seconds, minutes, hours, started;
 
 const initialValues = () => {
   milliseconds = 0;
@@ -23,24 +22,26 @@ const initialValues = () => {
 initialValues();
 
 const main = () => {
-  if (started == true) {
-    if (milliseconds == 100) {
+  if (started) {
+    if (milliseconds === 100) {
       seconds++;
       milliseconds = 0;
     }
-    if (seconds == 60) {
+
+    if (seconds === 60) {
       minutes++;
       seconds = 0;
     }
-    if (minutes == 60) {
+
+    if (minutes === 60) {
       hours++;
       minutes = 0;
     }
 
-    hoursSpan.innerText = hours > 9 ? hours : "0" + hours;
-    minutesSpan.innerText = minutes > 9 ? minutes : "0" + minutes;
-    secondsSpan.innerText = seconds > 9 ? seconds : "0" + seconds;
-    millisecondsSpan.innerText = milliseconds > 9 ? milliseconds : "0" + milliseconds;
+    hoursSpan.innerText = hours > 9 ? hours : `0${hours}`;
+    minutesSpan.innerText = minutes > 9 ? minutes : `0${minutes}`;
+    secondsSpan.innerText = seconds > 9 ? seconds : `0${seconds}`;
+    millisecondsSpan.innerText = milliseconds > 9 ? milliseconds : `0${milliseconds}`;
 
     milliseconds++;
   }
@@ -48,6 +49,6 @@ const main = () => {
 
 setInterval(() => main(), 10);
 
-document.getElementById("startBtn").addEventListener("click", () => (started = true));
-document.getElementById("stopBtn").addEventListener("click", () => (started = false));
-document.getElementById("resetBtn").addEventListener("click", () => initialValues());
+document.getElementById("startButton").addEventListener("click", () => (started = true));
+document.getElementById("stopButton").addEventListener("click", () => (started = false));
+document.getElementById("resetButton").addEventListener("click", () => initialValues());
